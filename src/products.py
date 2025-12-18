7b7f4fb9462eff73878e9b59af8a5924c7cf9218
Konstantin <erenegaaa136@gmail.com>
2025-12-18 11:58:53 +0300

    Update products.py

--- src/products.py
+++ src/products.py
@@ -8,7 +8,7 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
 
